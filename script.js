de566981af89b89b8eab058f09853533b8e6a5ec
John Permaloff <jvpermaloff@gmail.com>
2018-07-31 16:55:31 -0400

    day 2

--- script.js
+++ script.js
@@ -1,66 +1,35 @@
 " use strict";
 $(() => {
+    $(".guest-details").hide();
+    // $(".container").attr($(e.target).attr("id"), reservation)
+
+    let currentTable = null;
     $(".available").click((e) => {
-        $(".hideForm").toggle();
+        $("input").val("")
+        $(".hideForm").toggle()
+        $(".number").html((`Table Number: ${$(e.target).attr("id")}`))
     });
     $(".close").click((e) => {
-        $(".hideForm").toggle()        
-    });      
+        $(".hideForm").toggle()
+    });
     $(".save").click((e) => {
         $(".hideForm").toggle();
+        $("body").on("click", ".save", (e) => { currentTable.addClass("reserved")
+        .removeClass("available")
+        .data("guest-name", $("input").eq(0).val())
+        .data("guest-phone", $("input").eq(1).val())
+        .data("guest-size", $("input").eq(2).val())
+       
+        }) 
+    })
+    $(".available").click((e) => {
+        currentTable = $(e.target)                    
     });
-    $(".table").click((e) => {
-        $(e.target).addClass("reserved").removeClass("available");
-         
+    $("body").on("mouseenter mouseleave", ".reserved", (e) => {
+        $(e.target).css("cursor", "not-allowed")
+        $(".guest-details").fadeToggle(1000)
+        $(".guest-name").text(`Name: ${$(e.target).data("guest-name")}`)
+        $(".guest-phone").text(`Phone Number: ${$(e.target).data("guest-phone")}`)
+        $(".guest-size").text(`Party Size: ${$(e.target).data("guest-size")}`)
+        })
     });
-    $(".table").click((e) => {
-        if($(".close").on(click())) {
-            $(e.target).addClass("available").removeClass("reserved");;
-        } else if ($(".save").on(click())){
-        $(e.target).addClass("reserved").removeClass("available");
-        } 
-          
-    });
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// $(() => {
-
-//     $("body").on("click", "button", (e) => {
-//         $("section p").text($("input").val());
-//     });
-
-//     $("body").on("mouseenter", "section p", (e) => {
-//            console.log(e.target);
-//         $(e.target)
-//             .css("color", "orange")
-//             .fadeTo(2000, 0.25);
-//         $("body").on("mouseleave", "section p", (e) => {
-//             $(e.target)
-//                 .fadeTo(2000, 1, function () {
-//                     $(this).css("color", "black");
-//                 });
-
-
-//         });
-//     });
-//     $(".hidden").hide();
-//     $("body").on("mouseenter", ".toggle", () => {
-//         $(".hidden").toggle();
-//     });
-//     $("body").on("mouseleave", ".toggle", () => {
-//         $(".hidden").toggle();
-//     });
-
-// });
